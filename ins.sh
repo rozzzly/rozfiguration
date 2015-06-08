e9819c26b0c83828fecf8b99ce9e871415e0fca7
pclienau@gmail.com <pclienau@gmail.com>
2015-06-08 04:03:53 -0400

    include all that bullshit code from earlier

---
+++ ins.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd /home
+
+if [ ! -e _developers ]; then
+	mkdir _developers
+
+        chmod 770 _developers
+
+
+	chgrp -R developers _developers
+	chmod g+s -R _developers
+	
+	setfacl -dR -m u::rwx _developers # grant rwx (full acess) to user (owner of file
+	setfacl -dR -m g::rwx _developers # grant rwx (full access) to group
+	setfacl -dR -m o::--- _developers # grant --- (no access) to others
+
+	echo "setup-complete"
+fi
+
+cd _developers
+
+touch "$(whoami).htouch"
+echo "hello" >>  "$(whoami).pipe"
+vim "$(whoami).vim"
+ls -alhHp
