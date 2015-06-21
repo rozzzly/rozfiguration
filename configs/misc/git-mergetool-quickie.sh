cd3ba3fffb746fa1fa59dc900faeff15909378f8
rozzzly <pclienau@gmail.com>
2015-06-21 02:52:04 -0400

    derp

--- configs/misc/git-mergetool-quickie.sh
+++ configs/misc/git-mergetool-quickie.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-git config merge.tool vimdiff
-git config merge.conflictstyle diff3
-git config mergetool.prompt false
+# http://www.slashroot.in/how-to-install-and-configure-git
+
+git config --global merge.tool vimdiff
+git config --global merge.conflictstyle diff3
+git config --global mergetool.prompt false
 
 # TODO	make it auto apply itself
