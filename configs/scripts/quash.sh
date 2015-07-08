134132eb869f7f217b85cbf17aadf9b634fd08a9
rozzzly <pclienau@gmail.com>
2015-07-08 04:12:08 -0400

    finally got quash working

--- configs/scripts/quash.sh
+++ configs/scripts/quash.sh
@@ -18,15 +18,25 @@
 function quash ()
 {
 
-unset SSH_AUTH_SOCK
 
 IN=$1
+ARGV2=$2
+
+if [[ $IN == "reauth" ]] ; then
+	IN=$ARGV2
+	unset 'SSH_AUTH_SOCK'
+fi
+
 TTYL="${IN:=$(echo '30m')}"
+
+echo "TTYL is ${IN}"
 
 if [ -z "$SSH_AUTH_SOCK" ] ; then
 	eval `ssh-agent -s`
-	
     	ssh-add -t "$TTYL"
+else
+	echo "Already authed. If auth has expired, you need to reauth."
+	
 fi
 
 }
